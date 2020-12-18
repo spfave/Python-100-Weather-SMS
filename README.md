2e93cb88895322dc8673398eb0ea2c1adf6bc3fc
spfave <sebpfave@gmail.com>
2020-12-17 23:17:22 -0500

    use env variables and python-dotenv

--- README.md
+++ README.md
@@ -6,3 +6,15 @@ Course url: https://www.udemy.com/course/100-days-of-code/
 Section 35: Keys, Authentication, & Environment Variables
             SMS weather rain predictor app
 Use OpenWeather and Twilio API to check weather forecast for rain and send SMS message to alert if rain is predicted
+
+
+Notes:
+Environment variables in pipenv
+
+Native env variable calling only works with 'pipenv run' or in 'pipenv shell' at the moment. Use module python-dotenv to load .env file and environment variables when running file in terminal
+https://pypi.org/project/python-dotenv/
+
+General .env file questions
+https://stackoverflow.com/questions/51227159/how-do-i-set-environment-variables-in-pipenv
+https://stackoverflow.com/questions/59325669/set-environment-variables-when-activating-python-virtual-environment-in-windows
+https://pybit.es/persistent-environment-variables.html
--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+import os
+from dotenv import load_dotenv
 import requests
 from twilio.rest import Client
-from api_parameters import api_key_open_weather, twilio_sid, twilio_token, twilio_number, phone_number
+# from api_parameters import api_key_open_weather, twilio_sid, twilio_token, twilio_number, phone_number
+load_dotenv()
+
+# os.environ["TEST"] = "TEST_VAL"
+# print(os.getenv("TEST"))
+# print(os.getenv("MY_VAR"))
 
 
 # Functions
@@ -10,7 +17,8 @@ def get_weather_data(lat, lng):
         "lat": lat,
         "lon": lng,
         "exclude": "current,minutely,daily",
-        "appid": api_key_open_weather,
+        "appid": os.getenv("api_key_open_weather"),
+        # "appid": api_key_open_weather,
     }
     response = requests.get(url=api_open_weather, params=parameters)
     response.raise_for_status()
@@ -28,20 +36,20 @@ def check_hour_rain(weather_data):
 
 
 def send_rain_message():
-    client = Client(twilio_sid, twilio_token)
+    client = Client(os.getenv("twilio_sid"), os.getenv("twilio_token"))
     message = client.messages \
                     .create(
                         body="Looks like rain's in the 12 hour forcast, suggest grabbing an umbrella 🌧",
-                        from_=twilio_number,
-                        to=phone_number
+                        from_=os.getenv("twilio_number"),
+                        to=os.getenv("phone_number")
                     )
     print(message.status)
 
 
 # Main
-weather_data = get_weather_data(lat=38.880470, lng=-77.301872)
-# weather_data = get_weather_data(
-#     lat=40.760780, lng=-111.891045)  # test location with rain
+# weather_data = get_weather_data(lat=38.880470, lng=-77.301872)
+weather_data = get_weather_data(
+    lat=40.760780, lng=-111.891045)  # test location with rain
 weather_next_12_hours = weather_data["hourly"][:12]
 
 if check_hour_rain(weather_next_12_hours):
